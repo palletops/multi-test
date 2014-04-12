77e6e73c5a7def150549e68ad28c233fd324e550
Hugo Duncan <hugo@hugoduncan.org>
2014-04-11 21:31:31 -0400

    Update profiles for release

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,10 @@
 {:dev {:dependencies [[org.clojure/clojure "1.6.0-beta2"]]
        :plugins [[codox/codox.leiningen "0.6.4"]
                  [lein-marginalia "0.7.1"]
-                 [lein-set-version "0.4.1"]]}
- :no-checkouts {:checkout-shares ^:replace []} ; disable checkouts
+                 [lein-pallet-release "0.1.6"]]
+       :pallet-release
+       {:url "https://pbors:${GH_TOKEN}@github.com/palletops/multi-test.git",
+        :branch "master"}}
  :clojure-1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
  :doc {:dependencies [[com.palletops/pallet-codox "0.1.0"]]
        :codox {:writer codox-md.writer/write-docs
@@ -11,6 +13,4 @@
                :src-linenum-anchor-prefix "L"}
        :aliases {"marg" ["marg" "-d" "doc/source/0.1"]
                  "codox" ["doc"]
-                 "doc" ["do" "codox," "marg"]}}
- :release
- {:set-version {:updates [{:path "README.md" :no-snapshot true}]}}}
+                 "doc" ["do" "codox," "marg"]}}}
