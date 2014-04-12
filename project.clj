b3f02818b054ff65144814f71474ce485e729a2f
Hugo Duncan <hugo@hugoduncan.org>
2014-04-11 21:25:58 -0400

    Fix url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.palletops/multi-test "0.1.1-SNAPSHOT"
   :description "An alternative runner for clojure.test tests"
-  :url "http://palletops.com/multi-test"
+  :url "https://github.com/palletops/multi-test"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/core.async "0.1.267.0-0d7780-alpha"
